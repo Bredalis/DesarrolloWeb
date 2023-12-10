40c28c566c5f950dcc089581edc5e260773f5958
PRO X <bredalisgautreaux@gmail.com>
2023-12-10 08:33:28 -0400

    Nuevos archivos y carpeta - Archivos modificados

---
+++ Node.js/Basicos.js
@@ -0,0 +1,7 @@
+
+// Importar modulos
+var ruta = require("path");
+
+console.log("Nombre del directorio:", __dirname);
+console.log("Nombre del archivo:", __filename);
+console.log(ruta.basename(__filename));
