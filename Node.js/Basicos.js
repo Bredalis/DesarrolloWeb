51f54c0011b88fc82cce3083d6257d108589d872
PRO X <bredalisgautreaux@gmail.com>
2023-12-21 16:06:08 -0400

    Archivos actualizados

--- Node.js/Basicos.js
+++
@@ -1,7 +0,0 @@
-
-// Importar modulos
-var path = require("path");
-
-console.log("Nombre del directorio:", __dirname);
-console.log("Ruta del archivo:", __filename);
-console.log("Nombre del archivo:", path.basename(__filename));
--- Programas/LogicaDeProgramacion/index.js
+++
@@ -1,6 +0,0 @@
-
-// Mostrar el modal cuando la página se cargue
-window.onload = function () {
-    var myModal = new bootstrap.Modal(document.getElementById('modal-1'));
-    myModal.show();
-};
